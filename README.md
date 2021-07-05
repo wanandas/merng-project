45f33c2768a6f8cdfc6dd1defe704359254c7a51
BusyOnFriday <wanzerox@gmail.com>
2021-07-05 17:16:43 +0700

    first commit

---
+++ README.md
@@ -0,0 +1,3 @@
+## This project for learning
+
+Web Development with React.js, Apollo GraphQL, Typescript, Tailwind, Responsive Design, Node.js, Express.js, Nest.js, TypeORM, Docker, Docker Compose and VPS
---
+++ tailwind.config.js
@@ -0,0 +1,20 @@
+// tailwind.config.js
+module.exports = {
+  purge: ["./src/**/*/.{js,jsx,ts,tsx}", "./public/index.html"],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {},
+    screens: {
+      // NOTE  @media {min-width: xxx px} { ... }
+      sm: "640px",
+      md: "768px",
+      lg: "1024px",
+      xl: "1280px",
+      "2xl": "1536px",
+    },
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+};
